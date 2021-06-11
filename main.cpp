b26266619b5bb2ea282534a6dc89a6987cc0fa06
justin macklin <jmacklin308@gmail.com>
2021-06-11 15:41:49 -0500

    Added multiple size of room option and updated the output

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
  *
  * Requirements:
  *
- * Charges $40 per room
+ * Charges $40 per small room
+ * Charges $60 per large room
  * Sales tax rate is 6%
  * Estimates are valid for 30 days
  *
@@ -29,38 +30,58 @@ using namespace std;
 int main() {
 
     ///////Constants needed
-    const int costPerRoom {40};
+    const int costPerRoom_small {40};
+    const int costPerRoom_large {60};
     const float salesTax {0.06};
     const int estimateExpiry {30};
     //////
 
     //Intro
     cout << "Hello from Justin's Cleaning Service!" << endl;
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    cout << "Important info: " << endl;
-    cout << "Labor cost is $" << costPerRoom << " per room." << endl;
-    cout << "Sales tax is " << salesTax * 100 << "% " << endl;
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "Service Prices:  " << endl;
+    cout << "       $" << costPerRoom_small << " For a small room." << endl;
+    cout << "       $" << costPerRoom_large << " For a large room." << endl;
+    cout << "\n  !!   Sales tax is " << salesTax * 100 << "%   !! " << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 
 
     //first prompt
-    cout << "How many rooms will you need serviced?" << endl;
-    cout << "Enter Here: ";
-    int rooms {0};
-    cin >> rooms;
+    cout << "How many small rooms will you need serviced?" << endl;
+    int smallRooms {0};
+    cin >> smallRooms;
 
 
-    //calulate cost
-    int laborCost { rooms * costPerRoom };
-    float calculatedTax { laborCost * salesTax };
+    //Second prompt
+    cout << "How many large rooms will you need serviced?" << endl;
+    int largeRooms {0};
+    cin >> largeRooms;
+
+
+
+    //Calculate cost
+    int laborCost_small { smallRooms * costPerRoom_small };
+    int laborCost_large { largeRooms * costPerRoom_large };
+    int totalLabor { laborCost_large + laborCost_small };
+    float calculatedTax {  totalLabor * salesTax };
 
 
     //Display to the user the cost
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl;
-    cout << "Cost of labor is:          $" << laborCost << endl;
-    cout << "Sales tax is:              $" << calculatedTax << endl;
-    cout << "Total cost is:             $" << laborCost + calculatedTax << endl;
-    cout << "Estimates are valid for " << estimateExpiry << " days." << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl;
+    cout << "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "!!!! Estimates are valid for " << estimateExpiry << " days. !!!" << endl;
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "\n-----------------------------------------" << endl;
+    cout << "    " << smallRooms << " * Small Rooms        =   $" << laborCost_small << endl;
+    cout << "    " << largeRooms << " * Large Rooms        =   $" << laborCost_large << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "    Total Cost of labor:       $" << totalLabor << endl;
+    cout << "                           +    " << endl;
+    cout << "              Sales tax:       $" << calculatedTax << endl;
+    cout << "                           =    " << endl;
+    cout << "                  Total:       $" << totalLabor + calculatedTax << endl;
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl;
 
 
 
